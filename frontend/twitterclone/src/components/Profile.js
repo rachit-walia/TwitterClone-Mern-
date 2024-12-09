316c5f08d6bc70c064158fa4b0f856408f32e4f9
rachit-walia <rachitwalia3@gmail.com>
2024-12-09 09:34:48 +0530

    modified profile

--- frontend/twitterclone/src/components/Profile.js
+++ frontend/twitterclone/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdArrowBack } from "react-icons/io";
 import { Link, useParams } from "react-router-dom";
 import Avatar from "react-avatar";
@@ -9,6 +9,7 @@ import { USER_API_END_POINT } from "../utils/constant";
 import toast from "react-hot-toast";
 import { followingUpdate } from "../redux/userSlice";
 import { getRefresh } from "../redux/tweetSlice";
+import './Profile.css';
 
 const Profile = () => {
   const { user, profile } = useSelector((store) => store.user);
@@ -24,13 +25,11 @@ const Profile = () => {
         const res = await axios.post(`${USER_API_END_POINT}/unfollow/${id}`, {
           id: user?._id,
         });
-        console.log(res);
         dispatch(followingUpdate(id));
         dispatch(getRefresh());
         toast.success(res.data.message);
       } catch (error) {
         toast.error(error.response.data.message);
-        console.log(error);
       }
     } else {
       // Follow
@@ -39,77 +38,58 @@ const Profile = () => {
         const res = await axios.post(`${USER_API_END_POINT}/follow/${id}`, {
           id: user?._id,
         });
-        console.log(res);
         dispatch(followingUpdate(id));
         dispatch(getRefresh());
         toast.success(res.data.message);
       } catch (error) {
         toast.error(error.response.data.message);
-        console.log(error);
       }
     }
   };
 
   return (
-    <div className="w-[50%] border-l border-r border-gray-300 bg-white text-gray-800">
-      <div>
-        {/* Header */}
-        <div className="flex items-center py-2">
-          <Link
-            to="/"
-            className="p-2 rounded-full hover:bg-gray-100 hover:cursor-pointer"
-          >
-            <IoMdArrowBack size="24px" className="text-gray-600" />
-          </Link>
-          <div className="ml-2">
-            <h1 className="font-bold text-lg">{profile?.name}</h1>
-            <p className="text-gray-500 text-sm">10 posts</p>
-          </div>
+    <div className="profile-container">
+      <div className="profile-header">
+        <Link to="/" className="back-button">
+          <IoMdArrowBack size="24px" />
+        </Link>
+        <div className="profile-name">
+          <h1>{profile?.name}</h1>
+          <p>{profile?.posts?.length} posts</p>
         </div>
-        {/* Banner */}
+      </div>
+      <div className="banner">
         <img
           src="https://pbs.twimg.com/profile_banners/1581707412922200067/1693248932/1080x360"
           alt="banner"
-          className="w-full h-36 object-cover"
         />
-        {/* Avatar */}
-        <div className="absolute top-52 ml-2 border-4 border-white rounded-full">
-          <Avatar
-            src="https://pbs.twimg.com/profile_images/1703261403237502976/W0SFbJVS_400x400.jpg"
-            size="120"
-            round={true}
-          />
+      </div>
+      <div className="profile-pic-container">
+        <Avatar
+          src={profile?.profilePic}
+          size="120"
+          round={true}
+          className="profile-pic"
+        />
+      </div>
+      <div className="profile-info">
+        <h1>{profile?.name}</h1>
+        <p>@{profile?.username}</p>
+        <div className="followers-following">
+          <span>{profile?.followers?.length} Followers</span>
+          <span>{profile?.following?.length} Following</span>
         </div>
-        {/* Buttons */}
-        <div className="text-right m-4">
-          {profile?._id === user?._id ? (
-            <button className="px-4 py-1 hover:bg-gray-100 rounded-full border border-gray-400 text-gray-700">
-              Edit Profile
-            </button>
-          ) : (
-            <button
-              onClick={followAndUnfollowHandler}
-              className={`px-4 py-1 rounded-full ${
-                user.following.includes(id)
-                  ? "bg-gray-100 text-gray-700 border border-gray-400"
-                  : "bg-blue-500 text-white hover:bg-blue-400"
-              }`}
-            >
-              {user.following.includes(id) ? "Following" : "Follow"}
-            </button>
-          )}
-        </div>
-        {/* Profile Info */}
-        <div className="m-4">
-          <h1 className="font-bold text-xl">{profile?.name}</h1>
-          <p className="text-gray-500">{`@${profile?.username}`}</p>
-        </div>
-        {/* Bio */}
-        <div className="m-4 text-sm text-gray-700">
-          <p>
-            🌐 Exploring the web's endless possibilities with MERN Stack 🚀 | Problem solver by day, coder by night 🌙 | Coffee lover ☕ | Join me on this coding journey!
-          </p>
-        </div>
+      </div>
+      <div className="bio">
+        <p>{profile?.bio}</p>
+      </div>
+      <div className="follow-button-container">
+        <button
+          onClick={followAndUnfollowHandler}
+          className={`follow-button ${user.following.includes(id) ? "following" : ""}`}
+        >
+          {user.following.includes(id) ? "Following" : "Follow"}
+        </button>
       </div>
     </div>
   );
